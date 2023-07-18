b1f6ec133bc0b2df0cb25038f26e9633fd171176
Gabriel Vita <gabriel.vita83@gmail.com>
2023-07-18 15:25:10 -0300

    setup first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello boy</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#090909',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: '#fff',
+    fontSize: 46,
+    fontWeight: "bold",
+  }
+});
